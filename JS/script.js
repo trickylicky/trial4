124f5274ac8ffc69ea1e4a5544cac26ac578c574
Tricky <wangombevictor5@gmail.com>
2021-11-14 22:15:18 +0300

    add new JS

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,72 @@
-alert('please answer the question well!')
-console.log("shi there")
+ function akan(){
+          var d = new day()
+    var day = parseInt(document.getElementById("day").value);
+     var month = parseInt(document.getElementById("month").value);
+     var year = parseInt(document.getElementById("year").value);
+          if(day < 0 || day > 31){
+         document.getElementById("dayerror");
+         alert("invalid day input.Try again");
+     } else if (day === ""){
+         document.getElementById("dayerror");
+         alert("day is required");
+     } else {
+         d.setDay(day);
+     }
+     if (month < 0 || month > 12){
+         document.getElementByid("montherror");
+         alert("invalid month input.Try again");
+     } else if(month === ""){
+         document.getElementByid("montherror");
+         alert("field is required");
+     } else {
+         d.setMonth(month);
+     }
+ }
+    if (year.value === "" && year.value <= 0){
+        alert("enter valid year")
+    } else { d.setmonth
+    }
+    var days = ["sunday" , "monday" , "tuesday" , "wednesday","thursday","friday","saturday"]
+    var male =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var female =[" Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+    function getAkan(){
+        var ismale = document.getElementById('male').checked;
+        var isfemale = document.getElementById('female').checked;
+    
+    if (ismale== true &&isfemale == false){
+        if (day== 0){
+            alert("born on Sunday"+"your name is" + male[0]);
+        }
+        if (day== 1){
+            alert("born on Monday"+"your name is" + male[1]);}
+            if (day== 2){
+                alert("Born on Tuesday"+"your name is" + male[2]);}
+                if (day== 3){
+                    alert("Born on Wednesday"+"your name is" + male[3]);}
+                    if (day== 4){
+                        alert("Born on Thursday"+"your name is" + male[4]);}
+                                        if (day== 5){
+                                             alert("Born on Friday"+"your name is" +male[5]);}
+                                             if (day== 6){
+                                             alert("Born on Saturday"+"your name is" +male[6]);}
+    }
+    if (isfemale==true && ismale==false){
+        if (day== 0){
+            alert("Born on Sunday"+"your name is" + female[0]);}
+            if (day== 1){
+                alert("Born on Monday"+"your name is" + female[1]);}
+                if (day== 2){
+                    alert("Born on Tuesday"+"your name is" + female[2]);}
+                    if (day== 3){
+                        alert("Born on Wednesday"+"your name is" + female[3]);}
+                        if (day== 4){
+                            alert("Born on Thursday"+"your name is" + female[4]);}
+                            if (day== 5){
+                                alert("Born on friday"+"your name is" + female[5]);}
+                                if (day== 6){
+                                    alert("Born on Saturday"+"your name is" + female[6]);}
+    }
+
+
+}
